07b491e1915eb11574ad2b6ab09aea690c03e984
boz7 <zbratkovich@gmail.com>
2020-03-07 17:15:57 -0500

    added Materialize css & sidenav as well as test create user functionality

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import 'materialize-css/dist/css/materialize.min.css'
 import './index.css';
 import Sidenav from './components/Sidenav';
-import Form from './components/Home'
+import Home from './components/Home'
+
 
 // import * as serviceWorker from './serviceWorker';
-
 
 const router = (
       <Router>
             <Sidenav />
-            <div id="main">
                   <Route
                         path='/'
                         exact
                         component={Home}
                   />
-            </div>
       </Router>
 )
 export default router
