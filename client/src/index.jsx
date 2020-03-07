75235abc2fcb864123b13af019d0c206c163a9bf
boz7 <zbratkovich@gmail.com>
2020-03-07 11:23:01 -0500

    updating sidenav & forms

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './index.css';
 import Sidenav from './components/Sidenav';
 import Form from './components/Form'
@@ -9,10 +9,16 @@ import Form from './components/Form'
 
 
 const router = (
-<Router>
-      <Sidenav />
-      <Form />
-</Router>
+      <Router>
+            <Sidenav />
+            <div id="main">
+                  <Route
+                        path='/'
+                        exact
+                        component={Form}
+                  />
+            </div>
+      </Router>
 )
 export default router
 
