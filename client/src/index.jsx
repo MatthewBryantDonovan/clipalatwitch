b12ff02125a1b22378a2bb296957cd907803bde2
boz7 <zbratkovich@gmail.com>
2020-03-07 10:35:56 -0500

    adding compionent folder & import code

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route} from 'react-router-dom'
 import './index.css';
+import Sidenav from './components/Sidenav';
 // import * as serviceWorker from './serviceWorker';
 
 
