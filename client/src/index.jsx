de1820ccd7379097ffead403c011ca76b51a46a5
boz7 <zbratkovich@gmail.com>
2020-03-06 21:10:29 -0500

    initial commit to add boilerplate code & install packages we need

---
+++ client/src/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+import './index.css';
+// import * as serviceWorker from './serviceWorker';
+
+
+function router() {
+      return (
+            <Router>
+                  <Route>test</Route>
+            </Router>
+      )
+}
+
+export default index
+
+
+ReactDOM.render(router, document.getElementById('root'));
+
