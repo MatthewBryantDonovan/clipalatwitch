b09242caf30d65e29096d8b9e8b505528cfc4250
boz7 <zbratkovich@gmail.com>
2020-03-07 12:02:07 -0500

    adding post calls

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function Form() {
+function Form(props) {
+      const [username, setUsername] = useState('');
+      const [password, setPassword] = useState('');
+
+      function updateUsername(e) {
+            const newVal = e.target.val;
+            setUsername(newVal)
+      }
+     
+      function updatePassword(e) {
+            const newVal = e.target.val;
+            setPassword(newVal)
+      }
+
+      const submit = e => {
+            e.preventDefault;
+            props.submit(username, password)
+      }
       return (
             <React.Fragment>
-                  <form action="submit">
+                  <form action="submit" onSubmit={() => submit()} >
                         <label htmlFor="username">Username</label>
-                        <input type="text" id="username"/>
+                        <input type="text" id="username" onChange={() => updateUsername()} />
                         <label htmlFor="password">Password</label>
-                        <input type="password" id="password"/>
+                        <input type="password" id="password" onChange={() => updatePassword()} />
+                        <button type="submit" >Submit</button>
                   </form>
             </React.Fragment>
       )
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './index.css';
 import Sidenav from './components/Sidenav';
-import Form from './components/Form'
+import Form from './components/Home'
 
 // import * as serviceWorker from './serviceWorker';
 
@@ -15,7 +15,7 @@ const router = (
                   <Route
                         path='/'
                         exact
-                        component={Form}
+                        component={Home}
                   />
             </div>
       </Router>
