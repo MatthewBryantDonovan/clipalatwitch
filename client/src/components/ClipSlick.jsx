2cfcc3b6943463700d32bed5e0650f0f07fc1ca9
boz7 <zbratkovich@gmail.com>
2020-03-19 18:41:20 -0400

    adding

--- client/src/components/ClipSlick.jsx
+++ client/src/components/ClipSlick.jsx
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState }  from 'react';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
 function ClipSlick(props) {
+  const isClient = typeof window === 'object';
+  
+    function getSize() {
+      return isClient ? window.innerWidth : undefined;
+    }
+  
+    const [windowSize, setWindowSize] = useState(getSize);
+    const [slides, setSlides] = useState(3);
+
+  useEffect(() => {
+    getSize()
+    if(windowSize < 500) {
+      setSlides(1)
+    }
+  }, [windowSize])
     const settings = {
         dots: true,
         infinite: true,
         speed: 200,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        accessibility: true
+        slidesToShow: slides,
+        slidesToScroll: slides,
+        accessibility: true,
+        lazyLoad: true,
+        swipeToSlide: true,
       };
 
       return (
         <div className="center">
           <Slider {...settings}>
-          { (props.clipData) ? props.clipData.clips.map(clip => (
-                <div id={clip.id}>
+          { (props.clipData) ? props.clipData.clips.map((clip, index) => (
+                <div id={clip.id}  key={index}>
                     <iframe src={clip.embed_url + "&autoplay=false"} height="200" width="300"></iframe>
                 </div>
             )) : <div></div>}
