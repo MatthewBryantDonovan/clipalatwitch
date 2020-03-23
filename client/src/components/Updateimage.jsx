4d79585d6d436f7d351c111c7f119c939e656f3a
boz7 <zbratkovich@gmail.com>
2020-03-22 21:32:41 -0400

    fixing prevent defualt to the update image page

--- client/src/components/Updateimage.jsx
+++ client/src/components/Updateimage.jsx
@@ -1,25 +1,21 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import M from 'materialize-css/dist/js/materialize.min.js'
-import API from '../utils/API'
 
 function Updateimage(props) {     
-      const [modalInstance, setModalInstance] = useState(null)
-      const subClose = (e) => {
+      const subClose = e => {
             e.preventDefault()
-            setModalInstance(modalInstance.modal.close)
+            props.submitImageUrl()
       }
       useEffect(() => {
             const elems = document.querySelectorAll('.modal');
-            const instances = M.Modal.init(elems);
-            setModalInstance(instances)
-            
+            const instances = M.Modal.init(elems);  
       }, [])
 
       return (
             <React.Fragment>
                   <div id="modal1" class="modal" style={{ backgroundColor: "#161616" }}>
                         <div className="modal-content">
-                              <form action="submit" onSubmit={() => props.submitImageUrl()}>
+                              <form action="submit" onSubmit={subClose}>
                                     <div className="row">
                                           <div class="input-field col s6">
                                                 <input placeholder="https://www.some-image-url.com" id="first_name" type="text" class="validate" onChange={props.handleImageChange} required />
@@ -29,7 +25,7 @@ function Updateimage(props) {
                               </form>
                         </div>
                         <div class="modal-footer" style={{ backgroundColor: "#161616" }}>
-                              <button className="btn" type="submit" onClick={() => props.submitImageUrl()}>Submit</button>
+                              <button className="btn" type="submit" onClick={subClose}>Submit</button>
                               <a href="#!" className="modal-close waves-effect waves-green btn-flat" style={{ color: "#66fcf1" }}>Close</a>
                         </div>
                   </div>
